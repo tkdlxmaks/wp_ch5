4cdab074072e872fcc4fcfa7c173cf8e7a0afa31
WonHo <WonHo@DESKTOP-AV039T4>
2016-08-23 16:01:07 +0900

    first commit

---
+++ WebContent/hello.js
@@ -0,0 +1,7 @@
+	alert("hello World");
+	document.write("<h1>Hello World</h1>");
+	//window.location.href="http://www.sungkyul.ac.kr";
+	console.log("Hello World");
+	console.error("Error World");
+	console.warn("Warn World");
+	console.info("Info World");
